ac6093c4af4e462e24d2cd15ea2d78ddeac56ab7
Martin Calveira <martin.calveira@gmail.com>
2022-04-01 08:58:17 -0300

    Adding Update and Destroy actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @articles = Article.find(params[:id])
+    @article = Article.find(params[:id])
   end
 
   def new
@@ -19,6 +19,27 @@ class ArticlesController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
   end
 
   private
